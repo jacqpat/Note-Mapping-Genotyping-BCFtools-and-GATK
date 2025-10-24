5d08322d4eb5cbfa0de445ad78c020b73f20a597
jacqpat <82665557+jacqpat@users.noreply.github.com>
2025-10-24 16:21:44 +0200

    Create 11_run_eager_bwamem.sh
    Script EAGER I used.

---
+++ 11_run_eager_bwamem.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#SBATCH -J eager_Ssal_rescaled
+#SBATCH -p workq
+
+module purge
+module load bioinfo/NextflowWorkflows/nfcore-Nextflow-v21.10.6
+module load containers/singularity/3.9.9
+
+path_to_file=""
+path_to_ref=""
+path_to_config=""
+name_of_file="" # FASTQ.gz. Example if you have both forward and reverse reads: *{1,2}*.fastq.gz
+name_of_ref="" # .fa (or FASTA, etc.)
+name_of_idx="" # .fa.fai
+name_of_seq="" # .dict
+name_of_config="" # .config
+
+
+nextflow run nf-core/eager --input '${path_to_file}/${name_of_file}' \
+			   --single_end \
+         --fasta '${path_to_ref}/${name_of_ref}' \
+			   --fasta_index '${path_to_ref}/${name_of_idx}' \
+			   --seq_dict '${path_to_ref}/${name_of_seq}' \
+         -c '${path_to_config}/${name_of_config}' \
+			   --outdir './' \
+			   -profile genotoul \
+			   --skip_fastqc \
+			   --skip_adapterremoval \
+			   --skip_preseq \
+			   --clip_readlength 25 \
+			   --mapper 'bwamem'
+#			   -resume
+
+# NOTE:
+# bwa aln is the default mapper
+# remove --single_end if you have both forward and reverse reads.
+# remove "fasta_index" and "seq_dict" if no index and dict of seq.
+# add "bwa_index" if you have one (ex: if you've already run EAGER)
+# "resume" is an option to use if there was an error and you want to restart where you were.
+#
+# to keep discarded unmapped reads for later, do:
+# --run_bam_filtering
+# --bam_unmapped_type "bam"
